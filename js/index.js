752b0dae6134c8d90c4940ef5b9d077ea99c8337
Dmitriy <dmitriypruss21041984@gmail.com>
2021-06-01 20:07:57 +0300

    third commit

--- js/index.js
+++ js/index.js
@@ -22,9 +22,10 @@ class PassengerTransport extends Vehicle {
         this.passengerCount = passengerCount;
     };
     addPassenger() {
-        console.log('');
+        console.log('*************************');
         console.log(`${this.brand} ${this.model} - manufactured ${super.getAge()} days ago`);
         console.log(`passengerLimit - ${this.passengerLimit}, passengerCount - ${this.passengerCount}`);
+
         return this.passengerLimit - this.passengerCount > 0 ? !!this.passengerCount++ : false;
     }; 
 };
@@ -34,38 +35,61 @@ class FreightTransport extends Vehicle {
         super(dimensions, brand, model, manufactureDate);
         this.capacity = capacity;
     }
+    
+    // Решил немного разнообразить метод замыканием. Так показалось немного интереснее будет)
     checkLoadingPossibility() {
-        let loadLimit = 0;
-        let self = this;
+        let loaded = 0;
+        const self = this;
 
         return function(weight) {
-            loadLimit += weight;
-            
-            console.log('');
-            console.log('weight :>> ', weight, '| loadLimit :>> ', loadLimit, '| capacity :>> ', self.capacity);
+            if(typeof loaded !== 'number') {
+                return 'Full loading!';
+            };
 
-            return loadLimit < self.capacity;
+            loaded += weight;
+            
+            if(loaded > self.capacity) {
+                loaded = 'Full loading';
+            };
+
+            console.log('*************************');
+            console.log('weight :>> ', weight, '| loaded :>> ', loaded, '| capacity :>> ', self.capacity);
+
+            return loaded < self.capacity;
         };
     };     
 };
 
 
-const train = new Vehicle({a: 1200, b: 3, c: 5}, 'Druzhba', 'Meteor', new Date(1999, 11, 2));
-console.log('train.getMaxSize() :>> ', train.getMaxSize());
-console.log('train.getAge() :>> ', train.getAge());
+// testing...
+console.group('Testing class Vehicle')
+    const train = new Vehicle({a: 1200, b: 3, c: 5}, 'Druzhba', 'Meteor', new Date(1999, 11, 2));
+
+    console.log('train.getMaxSize() :>> ', train.getMaxSize());
+    console.log('train.getAge() :>> ', train.getAge());
+console.groupEnd();
+
+console.group('Testing class PassengerTransport');
+    const sprinter = new PassengerTransport({a: 5, b: 3, c: 2.2}, 'Mercedes', 'Sprinter', new Date(2015, 3, 1), 18, 17);
+
+    console.log('sprinter.addPassenger() :>> ', sprinter.addPassenger());
+    console.log('sprinter.addPassenger() :>> ', sprinter.addPassenger());
+console.groupEnd();
+
+console.group('Testing class FreightTransport');
+    const kamaz = new FreightTransport({a: 50, b: 4, c: 3}, 'Kamaz', 'Ultra', new Date(1974, 11, 2), 2000);
+    const kamazLoading = kamaz.checkLoadingPossibility();
+
+    console.log('kamazLoading(300) :>> ', kamazLoading(300));
+    console.log('kamazLoading(200) :>> ', kamazLoading(200));
+    console.log('kamazLoading(1100) :>> ', kamazLoading(1100));
+    console.log('kamazLoading(500) :>> ', kamazLoading(500));
+    console.log('kamazLoading(100) :>> ', kamazLoading(100));
+console.groupEnd();
 
 
-const sprinter = new PassengerTransport({a: 5, b: 3, c: 2.2}, 'Mercedes', 'Sprinter', new Date(2015, 3, 1), 18, 17);
-console.log('sprinter.addPassenger() :>> ', sprinter.addPassenger());
-console.log('sprinter.addPassenger() :>> ', sprinter.addPassenger());
 
 
-const kamaz = new FreightTransport({a: 50, b: 4, c: 3}, 'Kamaz', 'Ultra', new Date(1974, 11, 2), 2000);
-const kamazLoading = kamaz.checkLoadingPossibility();
-console.log('kamazLoading(300):>> ', kamazLoading(300));
-console.log('kamazLoading(200) :>> ', kamazLoading(200));
-console.log(' kamazLoading(1100):>> ', kamazLoading(1100));
-console.log('kamazLoading(500) :>> ', kamazLoading(500));
 
 
 
